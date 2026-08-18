5ca0f58a7d38f174ab22187804ffd5241607244a
Vicky <155694949+Vickychahar13@users.noreply.github.com>
2026-08-18 22:14:51 +0530

    Remove ignored files from repository

--- vendor/phanan/poddle/src/Enums/PodcastType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PhanAn\Poddle\Enums;
-
-enum PodcastType: string
-{
-    case EPISODIC = 'episodic';
-    case SERIAL = 'serial';
-}
--- vendor/phanan/poddle/tests/TestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tests;
-
-use Orchestra\Testbench\TestCase as BaseTestCase;
-
-class TestCase extends BaseTestCase
-{
-}
